702877537dc7080c719b02480dc3d3a2bdba59b5
markainick <scattaglia.nicola@gmail.com>
2016-10-27 14:40:06 +0200

    Italian Local

---
+++ lang/it/auth_ticket.php
@@ -0,0 +1,11 @@
+<?php
+
+$string['pluginname'] = 'Accesso diretto ticket';
+$string['auth_ticket'] = 'Accesso diretto ticket';
+$string['auth_tickettitle'] = 'Accesso diretto ticket';
+$string['auth_courseshopdescription'] = 'Questo metodo di autenticazione consente gli utenti di accedere a Moodle direttamente da un messaggio di notifica ricevuto via email. Il ticket criptato contiene tutte le informazioni necessarie per l\'identificazione dell\'utente e dell\'url che viene chiesto.';
+$string['tickettimeguard'] = 'Periodo Validità (ore)';
+$string['configtickettimeguard'] = 'Definisce l\'attesa di validità per il ticket. Dopodichè il ticket viene rifiutato.';
+$string['no'] = 'No';
+$string['yes'] = 'Sì (più compatibile)';
+
