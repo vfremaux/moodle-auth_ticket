edb2d18cf22feb1d159e1056a645cf4f977df1e8
valery <valery.fremaux@gmail.com>
2017-02-25 17:56:40 +0100

    Add tests

--- lang/fr/auth_ticket.php
+++ lang/fr/auth_ticket.php
@@ -17,24 +17,29 @@
 /**
  * @package     auth_ticket
  * @category    auth
- * @author      Valery Fremaux <valery@valeisti.fr>
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright   (C) 1999 onwards Martin Dougiamas  http://dougiamas.com
  * @copyright   (C) 2010 ValEISTI (http://www.valeisti.fr)
+ * @copyright   (C) 2012 onwards Valery Fremaux (http://www.mylearningfactory.com)
  *
- * implements an external access with encrypted access ticket for notification returns
+ * Implements an external access with encrypted access ticket for notification returns.
  */
 
-$string['pluginname'] = 'Accès direct par ticket';
 $string['auth_ticket'] = 'Accès direct par ticket';
+$string['auth_ticketdescription'] = 'Ce mode d\'authentification permet à des utilisteurs ayant reçu une notification par courriel de se connecter directement sur leur compte sans passer par la page de login. Le ticket crypté leur ayant été transmis contient toutes les informations suffisantes de login pendant une certaine durée de temps de validité. Au delà de cette durée le ticket est perdu.';
 $string['auth_tickettitle'] = 'Accès direct par ticket';
-$string['auth_courseshopdescription'] = 'Ce mode d\'authentification permet à des utilisteurs ayant reçu une notification par courriel de se connecter directement sur leur compte sans passer par la page de login. Le ticket crypté leur ayant été transmis contient toutes les informations suffisantes de login pendant une certaine durée de temps de validité. Au delà de cette durée le ticket est perdu.';
-$string['tickettimeguard'] = 'Temps de validité du ticket (en heures)';
+$string['configtesturl'] = 'Copiez cette url dans un navigateur non connecté.';
+$string['configlongtermtickettimeguard'] = 'Ce temps détermine la durée de validité d\'un ticket de notification à longue durée. Un accès avec un ticket plus âgé que cette durée est rejeté.';
 $string['configtickettimeguard'] = 'Ce temps détermine la durée de validité d\'un ticket de notification. Un accès avec un ticket plus âgé que cette durée est rejeté.';
+$string['configticketusessl'] = 'Si oui, le ticket est crypté/décrypté en utilisant les librairies openssl du système. Si non, c\'est la fonction d\'encryption interne de la base de données qui sera utilisée.';
 $string['decodeerror'] = 'Erreur de lecture du ticket';
 $string['encodeerror'] = 'Erreur d\'encodage du ticket';
+$string['longtermtickettimeguard'] = 'Temps de validité du ticket long (en jours)&nbsp;';
+$string['no'] = 'Non (Mysql et MariaDB uniquement)';
+$string['pluginname'] = 'Accès direct par ticket';
+$string['testurl'] = 'Url de test';
 $string['ticketerror'] = 'Erreur de désérialisaton du ticket';
-
-$string['no'] = 'Non';
+$string['tickettimeguard'] = 'Temps de validité du ticket court (en heures)&nbsp;';
+$string['usessl'] = 'Utiliser SSL pour crypter le ticket&nbsp;';
 $string['yes'] = 'Oui (plus compatible)';
 
