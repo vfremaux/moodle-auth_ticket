d2c01ee42b0d6fe4bda2ca2cda834238234609f8
valery <valery.fremaux@gmail.com>
2017-08-25 11:01:54 +0200

    code load for branch

--- version.php
+++ version.php
@@ -28,10 +28,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2017022500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016112900;        // Requires this Moodle version.
+$plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'auth_ticket';     // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.2.0 (Build 2017022500)';
+$plugin->release = '3.3.0 (Build 2017022500)';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0002';
+$plugin->codeincrement = '3.3.0002';
