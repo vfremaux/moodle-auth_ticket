6993c36f50e7fdfe17964e0c098073e2b0d2663f
valery <valery.fremaux@gmail.com>
2018-07-02 10:31:15 +0200

    add ionternal crypto method

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017022500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018070100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'auth_ticket';     // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.3.0 (Build 2017022500)';
-$plugin->maturity = MATURITY_RC;
+$plugin->release = '3.3.0 (Build 2018070100)';
+$plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.3.0003';
+$plugin->codeincrement = '3.3.0004';
