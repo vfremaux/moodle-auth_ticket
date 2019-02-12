457691545691dd1c8f8e59222d45bd2f18a8a10e
valery <valery.fremaux@gmail.com>
2019-02-13 00:46:30 +0100

    fix collision on new branch

--- version.php
+++ version.php
@@ -27,11 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018110900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018042700;        // Requires this Moodle version.
 $plugin->component = 'auth_ticket';     // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.5.0 (Build 2018110900)';
+$plugin->release = '3.6.0 (Build 2018110900)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.5.0005';
+$plugin->codeincrement = '3.6.0005';
