b00111bb7ddc2329dcac85d6337db21e1e6fd4f2
valery <valery.fremaux@gmail.com>
2020-03-02 11:03:12 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -28,10 +28,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2018110900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2019051100;        // Requires this Moodle version.
+$plugin->requires  = 2019111200;        // Requires this Moodle version.
 $plugin->component = 'auth_ticket';     // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.7.0 (Build 2018110900)';
+$plugin->release = '3.8.0 (Build 2018110900)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.7.0005';
+$plugin->codeincrement = '3.8.0005';
